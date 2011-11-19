3cab506c7dd34799c5be3cf33688d25f3db4a3af
Arunmozhi <aruntheguy@gmail.com>
2011-11-19 19:48:12 +0530

    added the suggest.php file

---
+++ suggest.php
@@ -0,0 +1,29 @@
+<?php
+
+   // Get the word
+     $part = $_GET['part'].'%';
+
+  // Connect to the database
+  include('config.php');
+
+  $con = mysql_connect($host,$user,$pass);
+
+  if (!$con){
+              echo "Unable to connect";
+             }
+  mysql_select_db($dbname,$con);
+
+  $response = array();
+  $words = array();
+
+  $query = mysql_query("SELECT * FROM raw_words WHERE word LIKE '{$part}'");
+  while($row=mysql_fetch_array($query))
+       {
+             $title=$row['word'];
+             $words[] = $title;
+        } 
+   $response['words'] = $words;
+   header("Content-type: application/json");
+   echo json_encode($response);
+
+?>
